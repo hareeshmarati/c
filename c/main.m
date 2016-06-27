03a8e2873a3a0a1c7df0d3a0dde157119b4cc937
Hareesh <harish.marati@facebook.com>
2016-06-27 15:50:19 +0530

    Initial Commit

---
+++ c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  c
+//
+//  Created by Hareesh Marati on 27/06/16.
+//  Copyright © 2016 Hareesh Marati. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
